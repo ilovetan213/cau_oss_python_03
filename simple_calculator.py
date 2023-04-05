e7143b53a04959a186709b1adb4c669041c0fd57
ilovetan213 <127828442+ilovetan213@users.noreply.github.com>
2023-04-06 03:09:02 +0900

    Add files via upload

---
+++ simple_calculator.py
@@ -0,0 +1,39 @@
+def arithmetic_ops(op):
+    a,b = map(int,input('숫자 두개를 입력해 주세요.').split())
+    return a, b, op(a,b)
+
+def plus():
+  a,b = map(int,input('숫자 두개를 입력해 주세요.').split())
+  c = a+b
+  print(c)
+
+def minus():
+  a,b = map(int,input('숫자 두개를 입력해 주세요.').split())
+  c = a-b
+  print(c)
+
+while True:
+    op = input("input operation:")
+
+    if op == '+':
+      plus()
+    elif op == '-':
+      minus()
+    elif op == "*":
+       a, b, ret = arithmetic_ops(lambda x,y:x*y)
+       print(ret)
+    elif op == "/":
+       a, b, ret = arithmetic_ops(lambda x,y:x/y)
+       print(ret)
+    elif op == "%":
+       a, b, ret = arithmetic_ops(lambda x,y:x%y)     
+       print(ret)
+    elif op == 'end':
+      break
+    else:
+      print("Invalid operation")
+      continue
+    
+
+
+print("Exit program")
